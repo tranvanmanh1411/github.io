2b18db530f6dd5ecf983754dc22c6251fee07695
tranvanmanh1411 <60852466+tranvanmanh1411@users.noreply.github.com>
2020-02-10 22:21:29 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -73,7 +73,7 @@ $('#no').click(() => {
 // generate text in input
 function textGenerate() {
     var n = "";
-    var text = " Tại vì cậu đáng yêu vl ý :<<<<<<< ";
+    var text = " Như cc ý :<<<<<<< ";
     var a = Array.from(text);
     var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
     var count = textVal.length;
