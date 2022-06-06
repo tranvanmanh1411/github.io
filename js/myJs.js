a8dd2abd2f91a4b99718f956afd8727162c5781b
tranvanmanh1411 <60852466+tranvanmanh1411@users.noreply.github.com>
2022-06-06 14:05:17 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -100,7 +100,7 @@ $('#yes').click(function() {
         html: true,
         width: 900,
         padding: '3em',
-        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyyy'>",
+        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='thả thính gõ vào đây'>",
         background: '#fff url("img/iput-bg.jpg")',
         backdrop: `
               rgba(0,0,123,0.4)
